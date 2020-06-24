34e9c029884d6887285aa6592fc21fdbb292980a
Simeon <simmeonn1@gmail.com>
2020-06-24 15:39:03 +0100

    Removed database related fields

--- Classes/BoardTile.cs
+++ Classes/BoardTile.cs
@@ -4,7 +4,6 @@ namespace Classes
 {
     public class BoardTile
     {
-        public int ID { get; set; }
         public int X { get; set; }
         public int Y { get; set; }
         public CharacterTile CharacterTile { get; set; }
--- Classes/CharacterTile.cs
+++ Classes/CharacterTile.cs
@@ -6,7 +6,6 @@ namespace Classes
 {
     public class CharacterTile
     {
-        public int ID { get; set; }
         public string Letter { get; set; }
         public int Score { get; set; }
     }
